b33ddcd606b431f5393b928a4d378fca3edb9356
alt3ralus <755@holbertonschool.com>
2019-10-01 17:51:09 +0000

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square():
+    """Defining an private instance attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__value = value
+
+    def area(self):
+        """
+        definig area
+        """
+        return self.__size**2
+
+    def my_print(self):
+        if self.size == 0 or self.size < 0:
+            print()
+        else:
+            for count in range(self.size):
+                print("#" * self.size)
